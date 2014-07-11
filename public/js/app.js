7cc6d8e60e4a1f866fbb699fcf033ba7838af1a7
Marcelo Alves <selfmadecelo@gmail.com>
2014-07-10 17:07:06 -0700

    Convert site from Angular to EmberJS
    Check out them paths though!

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,51 @@
-var app = angular.module('feedwatchReports',['ngResource']);
+App = Ember.Application.create({});
+
+App.Router.map(function() {
+  this.resource('reports', function() {
+    this.resource('report', { path: ':report_filename' });
+  });
+});
+
+App.ApplicationRoute = Ember.Route.extend({
+  model: function() {
+    return App.Reports.all();
+  }
+});
+
+App.ReportsRoute = Ember.Route.extend({
+  model: function() {
+    return App.Reports.all();
+  },
+
+  renderTemplate: function() {
+    this.render({ outlet: 'sidebar' });
+  }
+});
+
+App.ReportRoute = Ember.Route.extend({
+  model: function(params) {
+    return this.modelFor('reports').findBy('filename', params.report_filename);
+  }
+});
+
+App.SiderbarView = Ember.View.extend({
+model: function() {
+    return App.Reports.all();
+  },
+});
+
+App.Reports = Ember.Object.extend();
+
+App.Reports.reopenClass({
+  all: function() {
+      return $.getJSON("/public/reports.json").then(function(response) {
+        var items = [];
+ 
+        response.forEach( function (item) {
+          items.push( App.Reports.create(item) );
+        });
+ 
+          return items;
+      });
+  }
+});
--- public/js/controllers/reportlistctrl.js
+++
@@ -1,9 +0,0 @@
-function ReportListController($scope, $http) {
-    $http({method: 'GET', url: 'public/reports.json'}).success(function(data) {
-        $scope.reports = data; // response data 
-    });
-
-    $scope.select = function(report) {
-        $scope.selectedReport = report;
-    };
-}
